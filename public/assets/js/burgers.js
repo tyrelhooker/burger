4793bdc2d920dbde414f0fb42330a5f3f77e841c
T.J. Hooker <tyrelhooker@gmail.com>
2018-05-30 14:25:35 -0400

    create ajax calls in burgers.js

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,38 @@
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
+
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+    
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevoured
+    }). then (
+      function() {
+        console.log("Changed devoured state to: ", newDevoured);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    var newBurger = {
+      burger_name: $("#burg").val().trim(),
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created a new burger");
+        location.reload();
+      }
+    );
+  });
+});
