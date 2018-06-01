5378b6127135014c1f7b3bddfd5bd602ad08ec2b
T.J. Hooker <tyrelhooker@gmail.com>
2018-06-01 12:36:57 -0400

    working progress bar to add burger

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,22 +1,31 @@
 $(function() {
-  $(".change-devoured").on("click", function(event) {
-    event.preventDefault();
-    var id = $(this).data("id");
-    var newDevoured = $(this).data("newdevoured");
+  var totalBites = 4;
+  var currentBites = 0;
 
-    var newDevouredState = {
-      devoured: newDevoured
-    };
-    
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevouredState
-    }). then(
-      function() {
-        console.log("Changed devoured state to: ", newDevoured);
-        location.reload();
-      }
-    );
+  $(".change-devoured").on("click", function(event) {
+    // event.preventDefault();
+    if (currentBites < totalBites) {
+      currentBites++;
+      $('.progress-bar').css("width", Math.round(100 * currentBites / totalBites) + "%");
+      // $(this).siblings('.progress').find('.progress-bar').css("width", Math.round(100 * currentBites / totalBites) + "%");
+    } else if (totalBites === 4) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+  
+      var newDevouredState = {
+        devoured: newDevoured
+      };
+      
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }). then(
+        function() {
+          console.log("Changed devoured state to: ", newDevoured);
+          location.reload();
+        }
+      );
+    }
   });
 
   $(".create-form").on("submit", function(event) {
